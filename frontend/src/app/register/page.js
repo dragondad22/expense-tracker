5c9a1ff246dc81dcf9125fe98896d24539eaf841
ChrisThompson <dragondad22@gmail.com>
2025-01-24 11:00:53 -0600

    Added basic registration page

---
+++ frontend/src/app/register/page.js
@@ -0,0 +1,11 @@
+// src/app/register/page.js
+
+export default function RegisterPage() {
+    return (
+      <div>
+        <h1>Register</h1>
+        <p>Welcome to the registration page.</p>
+      </div>
+    );
+  }
+  
